1a5c7f3b6541f4b8c0c3d6c503ff1a909ae965fb
ishika2193 <ishikamangla19@gmail.com>
2026-07-16 13:38:11 +0530

    Add database setup script

---
+++ SQL/01_database_setup.sql
@@ -0,0 +1,13 @@
+/*
+=========================================================
+Database Setup
+Project: End-to-End E-commerce Analytics
+Dataset: Olist Brazilian E-commerce
+=========================================================
+*/
+
+CREATE DATABASE Sales;
+
+USE Sales;
+
+-- Dataset imported using LOAD DATA INFILE in cmd. 
